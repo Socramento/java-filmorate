2c1c3e87427632c30750e11d09c4c955c3964929
Maik Markov <maikgranata@yandex.ru>
2024-10-31 00:05:13 +0300

    PR 2.2

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -2,12 +2,16 @@ package ru.yandex.practicum.filmorate.storage;
 
 import jakarta.validation.Valid;
 import ru.yandex.practicum.filmorate.model.User;
+
 import java.util.List;
 
 
 public interface UserStorage {
-     List<User> findAll();
-     User findById(Long id);
-     User create(@Valid   User user);
-     User update(@Valid  User newUser);
+    List<User> findAll();
+
+    User findById(Long id);
+
+    User create(@Valid User user);
+
+    User update(@Valid User newUser);
 }
